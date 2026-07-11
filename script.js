01befc4725d028000c31ac72f6a44d7666e21a52
abhijithcr666tech <abhijithcr666@gmail.com>
2026-07-11 13:25:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+function analyzeMarket() {
 
+    let price = parseFloat(document.getElementById("price").value);
+    let signal = document.getElementById("signal").value;
+
+    if (isNaN(price)) {
+        document.getElementById("result").innerHTML =
+        "⚠️ Please enter market price";
+        return;
+    }
+
+    let target, stoploss;
+
+    if (signal === "BUY") {
+        target = price + 100;
+        stoploss = price - 50;
+    } else {
+        target = price - 100;
+        stoploss = price + 50;
+    }
+
+    document.getElementById("result").innerHTML =
+    `<h3>${signal} Signal</h3>
+     <p>Entry : ${price}</p>
+     <p>Target : ${target}</p>
+     <p>Stop Loss : ${stoploss}</p>`;
+}
